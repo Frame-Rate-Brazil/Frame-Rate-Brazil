bcbc227d96243710e96c0ce4b0bd8ad5bd4bddde
DaniloCosta1 <99129932+DaniloCosta1@users.noreply.github.com>
2025-09-01 10:53:50 -0300

    Update Login.jsx
    video 04 17min

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <section className="bg-base flex h-100 items-center">
       <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4 rounded-2xl">
         <h1 className="text-2xl font-bold">Faça seu login</h1>
-        <form action="" className="flex w-full flex-col gap-2">
+
+        <form action="" className="flex w-full flex-col gap-2" onSubmit={handleSubmit}>
           <input
             type="email"
             className="w-full rounded-full border border-gray-500 px-4 py-2"
             name=""
             id=""
             placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
           />
+          
           <input
             type="password"
             className="w-full rounded-full border border-gray-500 px-4 py-2"
             name=""
             id=""
             placeholder="Digite sua senha"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
           />
 
           <button className="bg-secondary-400 w-full cursor-pointer rounded-full border border-gray-500 px-4 py-2 text-base font-bold">
